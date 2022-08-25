bda0b7134c2439402781132573770f84e766d816
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-08-25 10:07:27 +0200

    bugfixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 setup(
     name='omnikeeper_importer',
-    packages=find_packages(include=['omnikeeper_importer']),
+    packages=['omnikeeper_importer'],
     version='1.0.0',
     description='Python library containing helper functions for implementing omnikeeper importers',
     author='Maximilian Csuk',
