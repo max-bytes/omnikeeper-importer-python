38ceb7473f379a4f16d4af8e62f481be0ec9d419
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-09-01 10:49:07 +0200

    Added healthcheck support

---
+++ omnikeeper_importer/healthcheck.py
@@ -0,0 +1,22 @@
+import os
+import time
+import sys
+from pathlib import Path
+
+def check():
+    try:
+        threshold_seconds = float(os.environ["HEALTHCHECK_THRESHOLD"]) # TODO: take from arg instead
+
+        epoch_time = time.time()
+        mtime = os.path.getmtime('/tmp/healthcheck_stat')
+        delta = epoch_time - mtime
+    except:
+        sys.exit(1)
+
+    if delta > threshold_seconds:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+def touch_stat_file():
+    Path('/tmp/healthcheck_stat').touch()
